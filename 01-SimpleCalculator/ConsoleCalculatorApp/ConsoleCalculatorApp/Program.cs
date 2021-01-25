c33abb412381f8fc17c574f2794e4acb26c00b6a
Joshhortt <joshhortt@yahoo.com>
2021-01-25 19:44:25 +0000

    First Commit. Add challenge 1 done.

---
+++ 01-SimpleCalculator/ConsoleCalculatorApp/ConsoleCalculatorApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculatorApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool exit = false; 
+
+			while(!exit)
+			{
+				Console.WriteLine("");
+				Console.WriteLine("Calculator");
+				Console.WriteLine("-----------------");
+				Console.WriteLine("1. Addition");
+				Console.WriteLine("2. Subtraction");
+				Console.WriteLine("3. Multiplication");
+				Console.WriteLine("4. Division");
+				Console.WriteLine("-----------------");
+				Console.WriteLine("0. Exit");
+				Console.WriteLine("-----------------");
+
+				Console.Write("Option: ");
+				string option = Console.ReadLine();
+				double a = 0;
+				double b = 0;
+
+				// exit aplication
+				if (option == "0") Environment.Exit(0);
+				// if (option == "0") exit = true;  is another possibility to exit the program.
+
+				// Ask for input values
+				Console.Write("A=");
+				a = double.Parse(Console.ReadLine());
+
+				Console.Write("B=");
+				b = double.Parse(Console.ReadLine());
+
+				// Execute the choosen arithmetic operation
+				if (option == "1")
+					Console.WriteLine(a + " + " + b + " = " + (a + b).ToString());
+
+				else if (option == "2")
+					Console.WriteLine(a + " - " + b + " = " + (a - b).ToString());
+
+				else if (option == "3")
+					Console.WriteLine(a + " x " + b + " = " + (a * b).ToString());
+
+				else if(option == "4")
+					Console.WriteLine(a + " : " + b + " = " + (a / b).ToString());
+
+			}
+		}
+	}
+}
