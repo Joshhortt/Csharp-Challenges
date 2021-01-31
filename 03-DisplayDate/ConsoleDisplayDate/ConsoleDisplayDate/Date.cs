1d01f9f7ce1d5972b906fba24e1b8aa21286fd2f
Joshhortt <joshhortt@yahoo.com>
2021-01-31 18:29:09 +0000

    First commit. Add challenge 3. Done.

---
+++ 03-DisplayDate/ConsoleDisplayDate/ConsoleDisplayDate/Date.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDisplayDate
+{
+	class Date
+	{
+		public int Month { get; set; }
+		public int Day { get; set; }
+		public int Year { get; set; }
+
+		public Date(int month, int day, int year)
+		{
+			Month = month;
+			Day = day;
+			Year = year;
+		}
+		public void DisplayDay()
+		{
+			Console.WriteLine("{0:D2}/{1:D2}/{2:D4}", Month, Day, Year);
+		}
+	}
+}
---
+++ 03-DisplayDate/ConsoleDisplayDate/ConsoleDisplayDate/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+//CHALLENGE 03: Create a class called Date that includes three pieces of information as automatic properties—a month (type int), 
+//a day(type int) and a year (type int). Your class should have a constructor that initializes the three automatic properties 
+//and assumes that the values provided are correct. Provide a method DisplayDate that displays the month, 
+//day and year separated by forward slashes (/). Write a test app named DateTest that demonstrates class Date’s capabilities.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDisplayDate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Date date = new Date(31, 01, 2021);
+			date.DisplayDay();
+
+			Console.ReadLine();
+		}
+	}
+}
