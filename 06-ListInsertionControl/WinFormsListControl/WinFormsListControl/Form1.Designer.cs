99920394da516aabe8a22476358b5f862991f967
Joshhortt <joshhortt@yahoo.com>
2021-02-11 19:42:53 +0000

    Adicionar primeiro commit com formulario que contem uma textBox, duas ListBox e um botao.

---
+++ 06-ListInsertionControl/WinFormsListControl/WinFormsListControl/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WinFormsListControl
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.textName = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(41, 84);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(167, 277);
+			this.listBox1.TabIndex = 0;
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.Location = new System.Drawing.Point(244, 84);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(170, 277);
+			this.listBox2.TabIndex = 1;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(244, 41);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(170, 23);
+			this.btnAdd.TabIndex = 2;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// textName
+			// 
+			this.textName.Location = new System.Drawing.Point(41, 41);
+			this.textName.Name = "textName";
+			this.textName.Size = new System.Drawing.Size(167, 20);
+			this.textName.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textName);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.listBox2);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.TextBox textName;
+	}
+}
+
---
+++ 06-ListInsertionControl/WinFormsListControl/WinFormsListControl/Program.cs
@@ -0,0 +1,27 @@
+﻿//Challenge 06: Control of insertion of names in two lists
+//if the name in list 1 does not exist, it is added.
+//if the name exists in list 1 and does not exist in list 2, it goes to list 2.
+//if the name exists in both lists, it is not added.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsListControl
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
