10fd5bab0ffcc04bcfe2a3573c9cd9bf68b50aea
Joshhortt <joshhortt@yahoo.com>
2021-02-24 19:51:16 +0000

    Add Challenge 09. Done!

---
+++ 09-ChangeMDIParentBackgroundColor/WinFormsChangeMDIParentBackgroundColor/WinFormsChangeMDIParentBackgroundColor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsChangeMDIParentBackgroundColor
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			foreach(Control c in this.Controls)
+				if(c is MdiClient)
+					c.BackColor = Color.Blue;
+				
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ 09-ChangeMDIParentBackgroundColor/WinFormsChangeMDIParentBackgroundColor/WinFormsChangeMDIParentBackgroundColor/Program.cs
@@ -0,0 +1,24 @@
+﻿// 09. Challenge -  how to change the background color of an Mdi Parent (Windows forms .Net Framework) Visual Studio Code
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsChangeMDIParentBackgroundColor
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
